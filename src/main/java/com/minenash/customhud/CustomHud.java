8067a06d7b833575e31e5ae85eba619a119d1f9d
Jakob <minenash@protonmail.com>
2024-04-29 20:33:53 -0400

    Fixed iris compat by removing iris (and sodium) compat

--- src/main/java/com/minenash/customhud/CustomHud.java
+++ src/main/java/com/minenash/customhud/CustomHud.java
@@ -5,7 +5,6 @@ import com.minenash.customhud.data.Crosshairs;
 import com.minenash.customhud.data.Profile;
 import com.minenash.customhud.errors.ErrorScreen;
 import com.minenash.customhud.errors.Errors;
-import com.minenash.customhud.mod_compat.BuiltInModCompat;
 import com.minenash.customhud.render.CustomHudRenderer;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
@@ -18,7 +17,6 @@ import net.minecraft.client.gui.screen.TitleScreen;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.toast.SystemToast;
 import net.minecraft.client.util.InputUtil;
-import net.minecraft.sound.SoundCategory;
 import net.minecraft.text.*;
 import net.minecraft.util.Formatting;
 import org.apache.logging.log4j.LogManager;
@@ -59,8 +57,6 @@ public class CustomHud implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-		BuiltInModCompat.register();
-
 		UpdateChecker.check();
 
 		HudRenderCallback.EVENT.register(CustomHudRenderer::render);
